ebf9f9c06b48ffb095523e0385f005c78a86135b
Julian Salcedo Torres <jsalcedo218@gmail.com>
2022-10-30 15:13:48 -0500

    Create quadratic_equation_coefficients.c

---
+++ codewars/8 kyu/quadratic_equation_coefficients.c
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5d59576768ba810001f1f8d6/c
+
+void quadratic(int x1, int x2, int coefficients[3]) {
+    coefficients[0] = 1;
+    coefficients[1] = -x1 + -x2;
+    coefficients[2] = x1 * x2;
+}
